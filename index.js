8524c4fa55af55e178ff09c30703465a45854b52
Morgan Frotten <mmfrotten@gmail.com>
2022-09-05 15:45:27 -0400

    completed dog feeder function

--- index.js
+++ index.js
@@ -19,8 +19,10 @@ Do the following:
 
    HINT: no function required
 */
-
-
+const votingAge = 18;
+if (votingAge >= 18){
+  console.log("true");
+}
 
 /*
 Task 1b - Values (not auto tested)
@@ -34,7 +36,12 @@ Do the following:
    HINT: no function required
 */
 
+const firstThing = 1;
+const secondThing = 2;
 
+if (firstThing === secondThing){
+
+}
 
 
 
@@ -49,6 +56,9 @@ Do the following:
    HINT: look up the Number method
 */
 
+const bestYear= "1999";
+Number(bestYear);
+console.log(bestYear);
 
 
 
@@ -61,10 +71,11 @@ Do the following:
    3. Multiply a and b and return the answer
 */
 
-function multiply(/*add your code here*/){
-  /*add your code here*/
+function multiply(a,b){
+  return a * b;
 }
 
+multiply(2, 4);
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -77,11 +88,11 @@ Do the following:
  3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/){
-  /*add your code here*/
+function dogYears(age){
+ return age * 7;
 }
 
-
+console.log(dogYears(3));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -129,10 +140,30 @@ NOTE 2: This is a great time to check the tests to see what it expects, versus w
         So, on this one test, the weight would be 4 pounds, and the age would be 1 years old. It's expecting your function to return a decimal number of 0.2
 */  
 
-function hungryDog(/*add your code here*/){
-  /*add your code here*/
+function hungryDog(weight, age) {
+if (age >= 1) {
+  if (weight <= 5) {
+    return weight * .05;
+  } else if (weight <= 10) {
+    return weight * .04;
+  } else if (weight <= 15) {
+    return weight *.03;
+  } else {
+    return weight * .02;
+  }
+} else {
+  if ((age >= (2/12)) && (age <= (4/12))) {
+    return weight * .1;
+  } else if (age <= (7/12)) {
+    return weight * .05;
+  } else if (age < 1) {
+    return weight * .04;
+  }
+  } 
 }
-
+  
+ console.log(hungryDog(4, 12))
+  
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
